e5c3c867245aaa39f538abd6f94fa4a65a14f03a
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-05-02 17:37:56 +0600

    recipes route created

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express()
 const port = 5000
 const cors = require('cors')
 const chefs = require('./Chefs.json')
+const recipes = require('./recipes.json')
 app.use(cors())
 app.get('/',(req,res)=>{
     res.send('Welcome to favourite food recipes')
 })
 app.get('/chefs',(req,res)=>{
     res.send(chefs);
+})
+app.get('/recipes',(req,res)=>{
+    res.send(recipes);
 })
 app.listen(port,()=>{
     console.log(`your req listening by ${port}`)
